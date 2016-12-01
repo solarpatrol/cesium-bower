bd0cc57f4c6e7f34ea099d197636ac09c9d2d134
Dmitriy Pushkov <ezze@ezze.org>
2016-12-02 01:40:11 +0300

    src directory is updated to 1.28.0.

--- src/Shaders/Builtin/Functions/unpackDepth.js
+++ src/Shaders/Builtin/Functions/unpackDepth.js
@@ -16,7 +16,7 @@ define(function() {
  {\n\
     // See Aras Pranckevičius' post Encoding Floats to RGBA\n\
     // http://aras-p.info/blog/2009/07/30/encoding-floats-to-rgba-the-final/\n\
-    return dot(packedDepth, vec4(1.0, 1.0 / 255.0, 1.0 / 65025.0, 1.0 / 160581375.0));\n\
+    return dot(packedDepth, vec4(1.0, 1.0 / 255.0, 1.0 / 65025.0, 1.0 / 16581375.0));\n\
  }\n\
 ";
 });
