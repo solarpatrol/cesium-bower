895895db1a45dcaf3bb40e99b73f1c870410d10d
Dmitriy Pushkov <ezze@ezze.org>
2018-01-09 10:22:01 +0300

    src directory is updated to 1.41.0.

---
+++ src/Shaders/Builtin/Constants/maxClippingPlanes.js
@@ -0,0 +1,13 @@
+//This file is automatically rebuilt by the Cesium build process.
+define(function() {
+    'use strict';
+    return "/**\n\
+ * The maximum amount of planes that can be clipped by czm_clipPlanes.\n\
+ *\n\
+ * @name czm_maxClippingPlanes\n\
+ * @glslConstant\n\
+ * @see czm_clipPlanes\n\
+ */\n\
+const int czm_maxClippingPlanes = 6;\n\
+";
+});
